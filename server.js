ec217c5e76c40849a38eb73aa88182f5c4414f53
Deniz Tetik <deniztetik31@gmail.com>
2016-07-01 15:42:30 -0400

    fixed server port issues

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 var app = require('./server-config.js');
 
-// app.set('port', (process.env.PORT || 3468));
-
-// var port = 4568;
+//var port = 4568;
 
 app.listen((process.env.PORT || 3468), function() {
   console.log('Server now listening on port ' + port);
 });
-
-
